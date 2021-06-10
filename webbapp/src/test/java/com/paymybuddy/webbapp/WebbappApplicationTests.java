c0a4dd56cf219489cf4d41719ba8b32ddb6e2658
Loic Benoit <loic@loicJava.home>
2021-06-10 18:53:18 +0200

    First commit - added springBoot and its dependencies

---
+++ webbapp/src/test/java/com/paymybuddy/webbapp/WebbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.webbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
